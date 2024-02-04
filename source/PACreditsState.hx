b355baaec3b8b1b689bae72d587b7421e80dcaff
Sang1278 <111049713+Sang1278@users.noreply.github.com>
2024-02-04 07:18:29 +0700

    I hope it work

--- source/PACreditsState.hx
+++ source/PACreditsState.hx
@@ -125,7 +125,7 @@ class PACreditsState extends MusicBeatState
 
 		creditSpr = new FlxSprite(0, 0);
 		add(creditSpr);
-		//creditSpr.scale.set(0.4, 0.4);
+		creditSpr.scale.set(1, 1);
 		creditSpr.updateHitbox();
 		creditSpr.screenCenter();
 		creditSpr.x -= 80;
@@ -236,7 +236,7 @@ class PACreditsState extends MusicBeatState
 	var holdTime:Float = 0;
 	override function update(elapsed:Float)
 	{
-		creditSpr.loadGraphic(Paths.returnGraphic('pacreditarts/' + people[curSelected][1] + FlxG.random.int(1, 1), null, true));
+		creditSpr.loadGraphic(Paths.returnGraphic('pacreditarts/' + people[curSelected][1], null, true));
 
 		if (FlxG.random.int(0, 1) < 0.01) 
 			{
@@ -273,8 +273,8 @@ class PACreditsState extends MusicBeatState
 		}
 
 		progress = FlxMath.lerp(progress, curSelected, CoolUtil.boundTo(elapsed * 20, 0, 1));
-		creditSpr.scale.x = FlxMath.lerp(creditSpr.scale.x, 0.4, CoolUtil.boundTo(elapsed * 3.8, 0, 1));
-		creditSpr.scale.y = FlxMath.lerp(creditSpr.scale.y, 0.4, CoolUtil.boundTo(elapsed * 3.8, 0, 1));
+		creditSpr.scale.x = FlxMath.lerp(creditSpr.scale.x, 1, CoolUtil.boundTo(elapsed * 3.8, 0, 1));
+		creditSpr.scale.y = FlxMath.lerp(creditSpr.scale.y, 1, CoolUtil.boundTo(elapsed * 3.8, 0, 1));
 	}
 
 	var targetY:Float;
