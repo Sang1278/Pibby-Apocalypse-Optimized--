43a4f4246d10831a9c38462328d11b372ed10da2
Idk1245 <93317189+Idk1245@users.noreply.github.com>
2024-02-21 00:18:07 +0000

    Add files via upload

--- assets/preload/stages/void/stage.hx
+++ assets/preload/stages/void/stage.hx
@@ -12,7 +12,7 @@ function onCreate()
     void.frames = retrieveAsset('images/void', 'atlas');
     void.animation.addByPrefix('idle', 'idle', 24, true);
     void.animation.play('idle');
-    void.setGraphicSize(Std.int(void.width * 1.7));
+    void.setGraphicSize(Std.int(void.width * 6.8));
     void.antialiasing = ClientPrefs.globalAntialiasing;
     void.scrollFactor.set(0.4, 0.4);
     void.updateHitbox();
@@ -21,13 +21,13 @@ function onCreate()
     glitch.alpha = 0.0001;
     glitch.x = 1000;
     glitch.y = 800;
-    glitch.scale.set(5, 5);
+    glitch.scale.set(20, 20);//16
     glitch.frames = retrieveAsset('images/gumballglitchbg', 'atlas');
     glitch.animation.addByPrefix('spin', 'spin', 15, false);
 
     house = new flixel.FlxSprite(0, -0);
     house.loadGraphic(retrieveAsset('images/Ilustracion_sin_titulo-2', 'image'));
-    house.setGraphicSize(Std.int(house.width * 2.5));
+    house.setGraphicSize(Std.int(house.width * 5));
     house.antialiasing = ClientPrefs.globalAntialiasing;
     house.scrollFactor.set(0.75, 1.06);
     house.updateHitbox();
@@ -35,7 +35,7 @@ function onCreate()
 
     rock = new flixel.FlxSprite(0, -200);
     rock.loadGraphic(retrieveAsset('images/Ilustracion_sin_titulo-3', 'image'));
-    rock.setGraphicSize(Std.int(rock.width * 2.5));
+    rock.setGraphicSize(Std.int(rock.width * 5));
     rock.antialiasing = ClientPrefs.globalAntialiasing;
     rock.scrollFactor.set(1, 1);
     rock.updateHitbox();
@@ -43,32 +43,10 @@ function onCreate()
 
     if (!ClientPrefs.lowQuality)
     {
-        rock2 = new flixel.FlxSprite(0, -200);
-        rock2.loadGraphic(retrieveAsset('images/Ilustracion_sin_titulo-4', 'image'));
-        rock2.setGraphicSize(Std.int(rock2.width * 2.5));
-        rock2.antialiasing = ClientPrefs.globalAntialiasing;
-        rock2.scrollFactor.set(1.3, 0.7);
-        rock2.updateHitbox();
-
-        rock3 = new flixel.FlxSprite(0, -200);
-        rock3.loadGraphic(retrieveAsset('images/Ilustracion_sin_titulo-5', 'image'));
-        rock3.setGraphicSize(Std.int(rock3.width * 2.5));
-        rock3.antialiasing = ClientPrefs.globalAntialiasing;
-        rock3.scrollFactor.set(0.9, 0.9);
-        rock3.updateHitbox();
-        rock3.color = houseColors[0];
-
-        rock4 = new flixel.FlxSprite(0, -200);
-        rock4.loadGraphic(retrieveAsset('images/Ilustracion_sin_titulo-6', 'image'));
-        rock4.setGraphicSize(Std.int(rock4.width * 2.5));
-        rock4.antialiasing = ClientPrefs.globalAntialiasing;
-        rock4.scrollFactor.set(0.65, 1.08);
-        rock4.updateHitbox();
-        rock4.color = houseColors[0];
-
+    
         wtf = new flixel.FlxSprite(0, -200);
         wtf.loadGraphic(retrieveAsset('images/Ilustracion_sin_titulo-7', 'image'));
-        wtf.setGraphicSize(Std.int(wtf.width * 2.5));
+        wtf.setGraphicSize(Std.int(wtf.width * 10));
         wtf.antialiasing = ClientPrefs.globalAntialiasing;
         wtf.scrollFactor.set(1, 1);
         wtf.updateHitbox();
@@ -76,36 +54,43 @@ function onCreate()
     }
 
     tweenLoopAngle(house, 4, -4, 6, 6);
-    if (!ClientPrefs.lowQuality) tweenLoopAngle(rock2, -2, 1.2, 2.5, 2.5);
-    if (!ClientPrefs.lowQuality) tweenLoopAngle(rock3, 360, 0, 30, 30);
+    
     if (!ClientPrefs.lowQuality) tweenLoopAngle(wtf, 2, -2, 5, 5);
 
-    if (!ClientPrefs.lowQuality) 
-    {
-        FlxTween.tween(rock4, {angle: 360}, 30, {
-        ease: FlxEase.sineInOut
-        });
-    }
+  
 
     if (!ClientPrefs.lowQuality) stupidFix = FlxTween.tween(wtf, {y: wtf.y}, 1);
     add(void);
     add(glitch);
-    if (!ClientPrefs.lowQuality) add(rock4);
-    if (!ClientPrefs.lowQuality) add(rock3);
-    if (!ClientPrefs.lowQuality) add(rock2);
     add(house);
     add(rock);
     if (!ClientPrefs.lowQuality) foreground.add(wtf);
 }
 
 function onCreatePost()
-{
+{  
+    PlayState.pibbyHealthbar.x += 140;
+    PlayState.pibbyHealthbar.y += 15;
+    PlayState.pibbyHealthbar.scale.set(2.5, 2.5);
+    if (!ClientPrefs.shaders) PlayState.pibbyHealthbar.color = PlayState.boyfriendColor;
+    
+    PlayState.boyfriend.y += 150;
+    PlayState.boyfriend.x += -130;
+    PlayState.gf.x -= 100;
+    PlayState.gf.y += 300;
     PlayState.boyfriend.origin.set(0, 200);
     PlayState.gf.origin.set(0, 200);
+    
+   
+    PlayState.camFollow.y += 200;
+    
 }
 
 function onSongStart()
 {
+    
+    
+    
     PlayState.boyfriend.color = charColors[0];
     PlayState.dad.color = charColors[0];
     PlayState.gf.color = charColors[0];
@@ -156,8 +141,7 @@ function onStepHit(curStep) {
 
         if (!ClientPrefs.lowQuality)
             {
-                rock3.color = houseColors[1];
-                rock4.color = houseColors[1];
+              
                 wtf.color = coolThingColors[1];
             }
     }
@@ -175,8 +159,7 @@ function onStepHit(curStep) {
 
         if (!ClientPrefs.lowQuality)
         {
-            FlxTween.color(rock3, 0.5, houseColors[1], houseColors[0], {ease: FlxEase.quadInOut } );
-            FlxTween.color(rock4, 0.5, houseColors[1], houseColors[0], {ease: FlxEase.quadInOut } );
+            
             FlxTween.color(wtf, 0.5, coolThingColors[1], coolThingColors[0], {ease: FlxEase.quadInOut } );
         }
 
@@ -196,8 +179,7 @@ function onStepHit(curStep) {
 
         if (!ClientPrefs.lowQuality)
         {
-            rock3.color = houseColors[1];
-            rock4.color = houseColors[1];
+            
             wtf.color = coolThingColors[1];
         }
     }
@@ -215,8 +197,7 @@ function onStepHit(curStep) {
 
         if (!ClientPrefs.lowQuality)
         {
-            rock3.color = houseColors[0];
-            rock4.color = houseColors[0];
+            
             wtf.color = coolThingColors[0];
         }
     }
@@ -234,8 +215,7 @@ function onStepHit(curStep) {
 
         if (!ClientPrefs.lowQuality)
             {
-                rock3.color = houseColors[1];
-                rock4.color = houseColors[1];
+                
                 wtf.color = coolThingColors[1];
             }
     }
@@ -253,8 +233,7 @@ function onStepHit(curStep) {
 
         if (!ClientPrefs.lowQuality)
         {
-            FlxTween.color(rock3, 1, houseColors[1], houseColors[0], {ease: FlxEase.quadInOut } );
-            FlxTween.color(rock4, 1, houseColors[1], houseColors[0], {ease: FlxEase.quadInOut } );
+            
             FlxTween.color(wtf, 1, coolThingColors[1], coolThingColors[0], {ease: FlxEase.quadInOut } );
         }
 
@@ -275,8 +254,7 @@ function onStepHit(curStep) {
 
         if (!ClientPrefs.lowQuality)
             {
-                rock3.color = houseColors[1];
-                rock4.color = houseColors[1];
+                
                 wtf.color = coolThingColors[1];
             }
     }
@@ -308,3 +286,14 @@ function onPause() {
 function onResume() {
     if (!ClientPrefs.lowQuality) stupidFix.active = true;
 }
+
+function onUpdate() {
+if (ClientPrefs.shaders) {
+PlayState.blurFNFZoomEdition.setFloat('PlayState.focusPower', 0);
+PlayState.blurFNFZoomEditionHUD.setFloat('PlayState.focusPower', 0);
+}
+}
+function goodNoteHit(note) {
+if (!ClientPrefs.shaders) PlayState.pibbyHealthbar.color = PlayState.scoreTxt.color;
+}    
+     
